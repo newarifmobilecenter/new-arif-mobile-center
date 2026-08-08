f2c8b658fb4e3e064dc85055e4e59aa3158101d1
newarifmobilecenter <zamzam9141@gmail.com>
2026-08-08 22:24:20 +0500

    Create oppo.ts

---
+++ app/tools/data/oppo.ts
@@ -0,0 +1,202 @@
+export const oppoTools = [
+  {
+    id: 1,
+    code: "Realme 6 Realme 6i Realme 7 Realme 6s Realme Narzo 20 Pro Realme Narzo 30",
+  },
+  {
+    id: 2,
+    code: "Realme XT Realme K5 Realme K1 Oppo Reno Z Oppo R15x Realme X2 Oppo K1",
+  },
+  {
+    id: 3,
+    code: "Realme C2 Oppo A1k",
+  },
+  {
+    id: 4,
+    code: "Oppo A3s Oppo A5 Realme 2 Realme C1 Oppo AX5 Oppo A12e",
+  },
+  {
+    id: 5,
+    code: "Oppo F1s Oppo A59 Old",
+  },
+  {
+    id: 6,
+    code: "Oppo A18 Oppo A38 Oppo A59 5G Oppo A57 5G Oppo A57s Oppo A57a Oppo A77 Oppo A77s Oppo K10 5G Oppo A17 Oppo A17k Oppo A57 2020 Oppo A58 5G Oppo A78 5G OnePlus Nord N20SE Oppo A58x 5G Oppo A56s 5G Oppo A57e Oppo A2x 5G Oppo A3b Oppo A1x 5G Oppo A2m 5G OnePlus Nord N300 5G Oppo A77 5G",
+  },
+  {
+    id: 7,
+    code: "Realme 2 Pro Realme U1 Oppo F9 Oppo F9 Pro",
+  },
+  {
+    id: 8,
+    code: "Oppo A53 Oppo A53s Oppo A53 2020 Oppo A54 4G Oppo A55 4G Oppo A11s Realme Narzo 20 Realme 7i Oppo A33 Oppo A54 Oppo A32 Realme C17 OnePlus Nord N100 Tray Oppo A55",
+  },
+  {
+    id: 9,
+    code: "Realme Narzo N53 Realme C53 Realme C51 Realme Note 50",
+  },
+  {
+    id: 10,
+    code: "Realme C30 Realme C30s Realme C33 Realme Narzo 50i Prime Realme C30F",
+  },
+  {
+    id: 11,
+    code: "Realme C25 Realme C25s Realme Narzo 50A Oppo A16 Oppo A16s Oppo A54s Oppo A54 Oppo A53s 5G Oppo A55 5G Oppo A56 5G Try C11 only Crown",
+  },
+  {
+    id: 12,
+    code: "Realme C11 Realme C12 Realme C15 Realme Narzo 20 Realme Narzo 30A Oppo A15 Oppo A15s Oppo A16e Oppo A16k Realme Q2i 5G Try A16 only Crown",
+  },
+  {
+    id: 13,
+    code: "Realme C20 Realme C11 2021 Realme Narzo 50i Realme C21",
+  },
+  {
+    id: 14,
+    code: "Realme Narzo 10A Realme Narzo 20A Tray Realme C3 Crown",
+  },
+  {
+    id: 15,
+    code: "Oppo A5s Oppo A7 Oppo A12 Oppo A11k Realme 3 Realme 3i Oppo A21 A20 A22 A24 CPH2083 CPH1901 RMX1825 RMX1827",
+  },
+  {
+    id: 16,
+    code: "Realme C55 Realme 11x 5G Oppo F23 5G Oppo A58 4G Realme N55 Oppo A98 5G OnePlus Nord CE3 Lite 5G Realme 11 5G Realme C67 5G Oppo K11x Oppo A1 5G Realme Narzo 60x 5G Oppo A79 5G",
+  },
+  {
+    id: 17,
+    code: "Realme 8i Realme 9i Oppo A96 Realme Narzo 50 Oppo K10",
+  },
+  {
+    id: 18,
+    code: "Oppo A52 Oppo A72 Oppo A92 Oppo A52 2020 Oppo A92 2020 Oppo A72 2020",
+  },
+  {
+    id: 19,
+    code: "Realme C35 Realme Narzo 50A Prime",
+  },
+  {
+    id: 20,
+    code: "Realme C21Y Realme C25Y Single Pata",
+  },
+  {
+    id: 21,
+    code: "Realme C3 Oppo A5 2020 Oppo A9 2020 Oppo A31 2020 Realme 5 Realme 5s Realme 5i Realme 10A Realme 20A",
+  },
+  {
+    id: 22,
+    code: "Oppo K3 Oppo Reno 2Z Oppo Reno 2F Realme X",
+  },
+  {
+    id: 23,
+    code: "Oppo A54 5G Oppo A74 5G Oppo A93 5G Realme Q3 OnePlus Nord N200",
+  },
+  {
+    id: 24,
+    code: "Realme 8 5G Realme 8s 5G Realme Narzo 30 5G Oppo A93 Realme Q3i Oppo A93s 5G Realme V13 5G Oppo K9x Realme 9 5G RMX3381 RMX3388 RMX3241",
+  },
+  {
+    id: 25,
+    code: "Oppo F19 Oppo F19 Pro Oppo F19s Oppo A95 4G Oppo A95 5G Oppo A94 5G Oppo A96 5G Oppo Reno 5Z Oppo Reno 7Z Oppo Reno 6Z Oppo Reno 8Z Oppo Reno 5F Oppo Reno 5 Lite Oppo Reno 6 Lite Oppo Reno 7 Lite Oppo Reno 8 Lite Oppo A94 4G Oppo A74 4G Realme X7 Realme X7 5G Realme 7 Pro Realme Q2 5G Realme V15 Oppo Reno 4 SE 5G Realme 8 Realme 8 Pro Oppo F21 Pro 5G CPH2219 CPH2223 CPH2285 CPH2211 CPH2237 CPH2343 CPH2203 RMX3092 RMX2170 RMX2176 RMX2173 RMX3085 RMX3082 CPH2341",
+  },
+  {
+    id: 26,
+    code: "Oppo F15 Oppo F17 Oppo A73 Oppo Reno 3 Oppo A91 Oppo K7 Oppo Reno 3 SE Find X2 Lite",
+  },
+  {
+    id: 27,
+    code: "Oppo A18 Oppo A57 New",
+  },
+  {
+    id: 28,
+    code: "Realme 9i 5G Realme 10 5G Realme 10s",
+  },
+  {
+    id: 29,
+    code: "Realme V25 Oppo K9s Realme Q5 OnePlus Nord CE2 Lite Realme 9 Pro 5G",
+  },
+  {
+    id: 30,
+    code: "Realme 12x 5G Realme C63 5G",
+  },
+  {
+    id: 31,
+    code: "OnePlus 10R 5G OnePlus 10T 5G Oppo Reno 8 Pro 5G OnePlus Ace Realme GT Neo 3",
+  },
+  {
+    id: 32,
+    code: "Realme 9 Pro 5G Realme 9 Pro OnePlus Nord CE2 Lite 5G RMX3471 CPH2381",
+  },
+  {
+    id: 33,
+    code: "Oppo Reno 7 4G Realme 11 4G Oppo Reno 7 5G Oppo Reno 8 4G Oppo Reno 8 5G Realme 9 4G Realme 9 Pro Plus 5G Realme 10 4G Oppo F21 Pro 4G Oppo F21s Pro 5G Oppo Find X5 Lite 5G OnePlus Nord CE2 5G Oppo A78 4G Oppo Reno 8T Realme Narzo 7 SE 5G Realme Narzo 50 Pro 5G",
+  },
+  {
+    id: 34,
+    code: "Oppo A60 Oppo A80 Realme C65 Realme Narzo N65 Oppo A3 Oppo A3X Oppo A3 Pro 5G Oppo K12X 5G",
+  },
+  {
+    id: 35,
+    code: "Oppo Reno 8 Pro 5G OnePlus 10R 5G OnePlus 10T 5G OnePlus Ace Realme GT Neo 3",
+  },
+  {
+    id: 36,
+    code: "Realme X SuperZoom Realme X50 5G Realme X3",
+  },
+  {
+    id: 37,
+    code: "Oppo A9 Oppo F11 Oppo A9X",
+  },
+  {
+    id: 38,
+    code: "Realme C63 4G Realme C61 Realme Narzo N63",
+  },
+  {
+    id: 39,
+    code: "OnePlus 9R OnePlus 8T",
+  },
+  {
+    id: 40,
+    code: "Realme 1 Oppo F5 Youth",
+  },
+  {
+    id: 41,
+    code: "Soon",
+  },
+  {
+    id: 42,
+    code: "OnePlus 4 Lite Oppo Reno 12F 4G Oppo Reno 12F 5G Realme Narzo 70 Pro Oppo F27 Oppo Reno 12FS 4G Oppo Reno 12FS 5G Realme 12 Plus Realme Narzo 70 Realme P1 5G Realme 12 Plus 5G Realme Narzo 70 5G Oppo Reno 12F Oppo Reno 12F 5G Realme P3 5G Oppo Reno 12FS Oppo Reno 13F Realme 13 Plus 5G Oppo F27 5G Realme 13 4G Realme 12 4G Realme Narzo 70 Turbo Realme P1 Speed 5G Realme 14 5G OnePlus Nord CE 4 Lite Realme Neo 7X Oppo K12X",
+  },
+  {
+    id: 43,
+    code: "Oppo Reno 5 4G Oppo Reno 5 5G Oppo Reno 6 5G Oppo Reno 6 4G OnePlus Nord 2 5G OnePlus Nord CE 5G OnePlus Nord 2T 5G Realme GT Master Edition Realme X7 Max 5G Realme GT Neo 2T 5G Realme GT Neo Fast Realme Q3 Pro Realme GT Realme GT Neo Oppo Find X3 Lite 5G",
+  },
+  {
+    id: 44,
+    code: "Realme 10 Pro Plus 5G Realme 11 Pro 5G Realme 11 Pro Plus 5G Realme 12 Pro 5G Realme 12 Pro Plus 5G Realme P1 Pro 5G Oppo F27 Pro Plus 5G Oppo Reno 11 5G Oppo Reno 10 5G Oppo Reno 10 Pro 5G Oppo Reno 11 Pro 5G Oppo Reno 8T 5G Realme Narzo 60 Pro 5G Oppo Reno 9 5G Oppo Reno 9 Pro 5G Oppo A1 Pro 5G",
+  },
+  {
+    id: 45,
+    code: "Oppo K12 OnePlus Ace 4 OnePlus Nord CE 3 OnePlus Nord CE 4 Oppo K11 Oppo Reno 11F Oppo F25 Pro 5G",
+  },
+  {
+    id: 46,
+    code: "Oppo A76 Oppo A36",
+  },
+  {
+    id: 47,
+    code: "Oppo Reno 5 OnePlus Nord 2",
+  },
+  {
+    id: 48,
+    code: "Realme 13 Pro Plus 5G OnePlus Nord CE4 Lite Realme 12 4G",
+  },
+  {
+    id: 49,
+    code: "Oppo K12X OnePlus Nord CE4 Lite 5G Oppo F27 5G Oppo Reno 12F Oppo Reno 12FS Oppo Reno 12FS 5G Oppo Reno 12F 5G Realme 12 Realme 13 Plus 5G Realme Narzo 70 Turbo Realme 13 Pro Realme 13 4G Oppo Reno 13F Oppo Reno 13F 5G",
+  },
+  {
+    id: 50,
+    code: "Realme P3X Realme 14X Realme C75 4G Realme C75 5G Realme C75X",
+  },
+];
