fa26a2fc01cf873547e57e7317d4bc7f4747ee8e
newarifmobilecenter <zamzam9141@gmail.com>
2026-08-08 22:25:06 +0500

    Create Samsung.ts

---
+++ app/tools/data/Samsung.ts
@@ -0,0 +1,146 @@
+export const samsungTools = [
+  {
+    id: 1,
+    code: "Samsung F22 Samsung A22 4G Samsung M22",
+  },
+  {
+    id: 2,
+    code: "Samsung A12 Samsung M12 Samsung F12 Samsung A02 Samsung M02 Samsung A32 5G Samsung M23 5G Samsung A125 Samsung M127 Samsung A022 Samsung M022 Samsung A326 Samsung M326",
+  },
+  {
+    id: 3,
+    code: "Samsung M11 Samsung A11",
+  },
+  {
+    id: 4,
+    code: "Samsung A04s Samsung A23 Samsung A13 5G Samsung A136 Samsung A047 Samsung A047F",
+  },
+  {
+    id: 5,
+    code: "Samsung A04e Samsung A02s Samsung A03s Samsung M02s Samsung A03 Samsung A04 Samsung F04 Samsung F02s",
+  },
+  {
+    id: 6,
+    code: "Samsung J6 Plus Samsung J4 Plus",
+  },
+  {
+    id: 7,
+    code: "Samsung A10 Samsung M10 Samsung A10s",
+  },
+  {
+    id: 8,
+    code: "Samsung A30 Samsung A50 Samsung A30s Samsung A50s",
+  },
+  {
+    id: 9,
+    code: "Samsung A15 Samsung A15 5G Samsung F15 Samsung M15",
+  },
+  {
+    id: 10,
+    code: "Samsung M21 Samsung M21s Samsung M30 Samsung M30s Samsung M31 Samsung F41 Samsung M31s Samsung M305 Samsung M315 Samsung M307 Samsung F415",
+  },
+  {
+    id: 11,
+    code: "Samsung F62 Samsung M62 Samsung A71 4G Samsung A71 5G Samsung M51",
+  },
+  {
+    id: 12,
+    code: "Samsung A70 Samsung A70s Samsung A707 Samsung A705",
+  },
+  {
+    id: 13,
+    code: "Samsung A20 Samsung A20s",
+  },
+  {
+    id: 14,
+    code: "Samsung A60 Samsung M40",
+  },
+  {
+    id: 15,
+    code: "Samsung A01 Core Samsung M01 Core Samsung A013 Samsung M013",
+  },
+  {
+    id: 16,
+    code: "Samsung A24 Samsung A25 Samsung F34 Samsung M34",
+  },
+  {
+    id: 17,
+    code: "Samsung A32 4G Samsung M32 4G",
+  },
+  {
+    id: 18,
+    code: "Samsung M01 Samsung A01 Samsung M015 Samsung A015",
+  },
+  {
+    id: 19,
+    code: "Samsung A05s Samsung M14 4G Samsung A057",
+  },
+  {
+    id: 20,
+    code: "Samsung A13 Samsung A13 4G Samsung A13 Lite Samsung F13 Samsung M13 Samsung A135 Samsung A137",
+  },
+  {
+    id: 21,
+    code: "Samsung A14 4G Samsung A14 5G Samsung A146 Samsung A146B Samsung A145F",
+  },
+  {
+    id: 22,
+    code: "Samsung M14 5G Samsung F14 5G Samsung M146B Samsung E146B",
+  },
+  {
+    id: 23,
+    code: "Samsung A15 5G Samsung A15 4G Samsung F15 5G Samsung M15 5G",
+  },
+  {
+    id: 24,
+    code: "Samsung A16 5G Samsung A16 4G Samsung A26 5G Samsung A166P",
+  },
+  {
+    id: 25,
+    code: "Samsung A22 5G Samsung F42 5G Samsung A22s 5G",
+  },
+  {
+    id: 26,
+    code: "Samsung A23 4G Samsung A23 5G Samsung F23 5G Samsung M23 5G Samsung M33 5G",
+  },
+  {
+    id: 27,
+    code: "Samsung A24 Samsung A25 5G Samsung M34 5G",
+  },
+  {
+    id: 28,
+    code: "Samsung A32 4G Samsung M32 4G",
+  },
+  {
+    id: 29,
+    code: "Samsung A51 Samsung A51 5G Samsung M31s",
+  },
+  {
+    id: 30,
+    code: "Samsung A52 5G Samsung A52 4G Samsung A52s 4G",
+  },
+  {
+    id: 31,
+    code: "Samsung A53 5G Samsung A53 4G",
+  },
+  {
+    id: 32,
+    code: "Samsung A55 5G Samsung A35 5G Samsung M35 5G",
+  },
+  {
+    id: 33,
+    code: "Samsung M52 5G Samsung M53 5G Samsung M54 5G Samsung F54 5G Samsung M52 Samsung M54 Samsung M53",
+  },
+  {
+    id: 34,
+    code: "Samsung M55 5G Samsung F55 5G Samsung C55 5G Samsung M55s 5G",
+  },
+  {
+    id: 35,
+    code: "Samsung S20 FE 4G Samsung S20 FE 5G",
+  },
+  {
+    id: 36,
+    code: "Samsung A06 5G Samsung F06 5G Samsung M06 5G Samsung A066B Samsung E066B Samsung M066B",
+  },
+];
