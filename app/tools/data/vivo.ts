b6c2cee66d5a8d1ac24fdb71dda426b6b5db6506
newarifmobilecenter <zamzam9141@gmail.com>
2026-08-08 22:23:33 +0500

    Create data/vivo.ts

---
+++ app/tools/data/vivo.ts
@@ -0,0 +1,178 @@
+export const vivoTools = [
+  {
+    id: 1,
+    code: "Vivo Y11 Vivo Y12 Vivo Y15 Vivo Y17 Vivo U10 Vivo Y3",
+  },
+  {
+    id: 2,
+    code: "Vivo T3x 5G Vivo Y200i 5G Vivo Y58 5G iQOO Z9x",
+  },
+  {
+    id: 3,
+    code: "Vivo Y20 Vivo Y20g Vivo Y12g Vivo Y01",
+  },
+  {
+    id: 4,
+    code: "Vivo Y19 Vivo U20 Vivo Y5s Vivo U3x iQOO Z5i",
+  },
+  {
+    id: 5,
+    code: "Vivo Y03 Vivo Y18 Vivo T3 Lite 5G Vivo Y28s 5G Vivo Y18e Vivo Y18i Vivo Y18s Vivo Z9 Lite 5G Vivo Y28e 5G Vivo Y03t",
+  },
+  {
+    id: 6,
+    code: "Vivo Y22 Vivo Y17s Vivo Y22s Vivo Y22 New Vivo Y28 5G Vivo Y33t Vivo Y36i Vivo Y36 Vivo Y12 New",
+  },
+  {
+    id: 7,
+    code: "Vivo Y91 Vivo Y91i Vivo Y90 Vivo Y91c Vivo Y93 Vivo Y95 Vivo Y1s",
+  },
+  {
+    id: 8,
+    code: "Vivo Y79 Vivo Y7 Plus Vivo Y79a",
+  },
+  {
+    id: 9,
+    code: "Vivo Y02 Vivo Y02t Vivo Y02 New Vivo Y02a",
+  },
+  {
+    id: 10,
+    code: "Vivo Y71 Vivo Y71i Vivo V7",
+  },
+  {
+    id: 11,
+    code: "Vivo S1 Vivo S1 Pro Vivo Z1x Vivo Y7s",
+  },
+  {
+    id: 12,
+    code: "Vivo V17 Vivo V19 Neo",
+  },
+  {
+    id: 13,
+    code: "Vivo Z1 Pro Vivo Z5x",
+  },
+  {
+    id: 14,
+    code: "Vivo V28 Vivo Y73 Vivo V23e 5G Vivo Y75 4G",
+  },
+  {
+    id: 15,
+    code: "Vivo V20 Vivo V21e 4G Vivo V21e 5G Vivo V20 2021 Vivo V21 4G Vivo V23e Vivo Y73 4G Vivo S6 Vivo Y75 4G iQOO Z6 4G",
+  },
+  {
+    id: 16,
+    code: "OLED ONLY Vivo V20 Vivo V23e Vivo Y75 Vivo V21e Vivo Y70 Vivo X50 Lite Vivo Y55 Vivo Y51 OLED",
+  },
+  {
+    id: 17,
+    code: "Vivo T1 Pro Vivo V25e 5G Vivo V25 5G Vivo V21 5G Vivo S15e Vivo S9e",
+  },
+  {
+    id: 18,
+    code: "Vivo V11 Vivo V11i Vivo Z3i Vivo Y97 Vivo Z3",
+  },
+  {
+    id: 19,
+    code: "Vivo Y81 Vivo Y81i Vivo Y83 Vivo Y83 Pro Vivo Y81s",
+  },
+  {
+    id: 20,
+    code: "Vivo V9 Vivo V9 Youth Vivo V9 Pro Vivo Y85",
+  },
+  {
+    id: 21,
+    code: "Vivo Y21 Vivo Y21s Vivo Y21a Vivo Y21e Vivo Y21t Vivo Y21g Vivo Y02s Vivo Y16 Vivo Y30 5G Vivo Y15a Vivo Y15c Vivo Y32 Vivo Y33 5G",
+  },
+  {
+    id: 22,
+    code: "Vivo Y51 2020 Vivo Y31 2020 Vivo Y73 Vivo Y72s Vivo Y53s iQOO Z3 iQOO U3 iQOO U3x Vivo Y33s Vivo Y31 Vivo Y15a Vivo T2x Vivo Y72 5G Vivo Y55s 5G Vivo Y76 5G Vivo Y76s 5G Vivo Y75 5G Vivo Y77 5G Vivo Y56 5G",
+  },
+  {
+    id: 23,
+    code: "Vivo Y20 Vivo Y20a Vivo Y20t Vivo Y20s Vivo Y20i Vivo Y20g Vivo Y12g Vivo Y20sg Vivo Y12s Vivo Y12a Vivo Y20e Vivo Y30g Vivo Y31s 5G Vivo Y15a Vivo Y11s Vivo Y12d Vivo Y20g Vivo Y12s Vivo Y15s Vivo Y01 iQOO U1x",
+  },
+  {
+    id: 24,
+    code: "Vivo T2x 5G Vivo Y56 New Vivo Y55s Vivo Y77 5G Vivo Y72 5G Vivo Y72t Vivo Y56 5G Vivo Y75 5G Vivo T1 5G iQOO Z6 5G iQOO Z6 Lite 5G",
+  },
+  {
+    id: 25,
+    code: "Vivo Y27 4G Vivo Y36 4G",
+  },
+  {
+    id: 26,
+    code: "Vivo Y35 Vivo Y35 New",
+  },
+  {
+    id: 27,
+    code: "Vivo V21e 5G Vivo S10e Vivo S6 Vivo S7e Vivo Y73s Vivo Y71t Vivo Y55 4G Vivo X50 Vivo G1",
+  },
+  {
+    id: 28,
+    code: "Vivo Y03 Vivo Y18",
+  },
+  {
+    id: 29,
+    code: "Vivo Y28 4G Vivo Y38 5G Vivo Y19s",
+  },
+  {
+    id: 30,
+    code: "Vivo V27 5G Vivo S16 Vivo Y78 Plus Vivo V29e Vivo V29 Lite 5G Vivo Y200 Pro Vivo T2 Pro 5G Vivo V27 Pro 5G Vivo V29 SE 5G Vivo Y78 5G Vivo V30e 5G Vivo Y300 Plus 5G iQOO Z7 Pro Vivo S16 Pro Vivo S17e",
+  },
+  {
+    id: 31,
+    code: "Vivo Y30 4G Vivo Y50 Only Crown",
+  },
+  {
+    id: 32,
+    code: "Vivo V5 Vivo Y67",
+  },
+  {
+    id: 33,
+    code: "Vivo Y78T Vivo Y78 Vivo Z8 Vivo Z8x Vivo Y100i iQOO Z7x Vivo Y36 5G Vivo Y36",
+  },
+  {
+    id: 34,
+    code: "Vivo V15 Pro Vivo X27",
+  },
+  {
+    id: 35,
+    code: "Vivo V11 Pro Vivo X23",
+  },
+  {
+    id: 36,
+    code: "Vivo V20 Pro Vivo S7 Vivo S7t",
+  },
+  {
+    id: 37,
+    code: "Vivo V23 5G Vivo S9 Vivo S10 Vivo S10 Pro Vivo S12 Pro",
+  },
+  {
+    id: 38,
+    code: "Vivo X60 Vivo X70 Vivo V2045",
+  },
+  {
+    id: 39,
+    code: "Vivo iQOO 007 Vivo iQOO Neo 5s Vivo iQOO Neo 5 Vivo S15",
+  },
+  {
+    id: 40,
+    code: "Vivo Y100 Vivo Y100A Vivo Y100 T2 5G iQOO Z7 5G",
+  },
+  {
+    id: 41,
+    code: "Vivo Y200 5G Vivo Y200e 5G Vivo Y100 Vivo T3 5G Vivo V29e",
+  },
+  {
+    id: 42,
+    code: "Vivo Y52s Vivo Y75 5G Vivo Y72 iQOO Z3 Vivo Y51 Vivo Y53s",
+  },
+  {
+    id: 43,
+    code: "Vivo T4x 5G iQOO Z10x 5G Vivo T3x 5G Vivo Y58 5G iQOO 29x Vivo Y200i 5G",
+  },
+  {
+    id: 44,
+    code: "Vivo V40e Vivo T3 Pro iQOO Z9s iQOO Z9s Pro",
+  },
+];
